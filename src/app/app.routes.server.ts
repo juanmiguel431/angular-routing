6aad39d43964dc585b7d96b15c4aa1cbc98a4be7
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-07-16 18:35:25 -0400

    Update SSR script and adjust server routes configuration

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  // {
+  //   path: 'users/:userId/tasks',
+  //   renderMode: RenderMode.Prerender,
+  //   async getPrerenderParams() {
+  //     const ids = ['u1', 'u2', 'u3'];
+  //     return ids.map((id) => ({ userId: id }));
+  //   },
+  // },
+  {
     path: '**',
-    renderMode: RenderMode.Server
-  }
+    renderMode: RenderMode.Server,
+  },
 ];
