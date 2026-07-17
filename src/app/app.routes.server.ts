34714a21f1f94ed343ab90100207ecff72f52c5e
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-07-16 20:06:21 -0400

    Uncomment and enable prerendering for `users/:userId/tasks` route in server configuration

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -5,14 +5,14 @@ export const serverRoutes: ServerRoute[] = [
     path: '',
     renderMode: RenderMode.Prerender,
   },
-  // {
-  //   path: 'users/:userId/tasks',
-  //   renderMode: RenderMode.Prerender,
-  //   async getPrerenderParams() {
-  //     const ids = ['u1', 'u2', 'u3'];
-  //     return ids.map((id) => ({ userId: id }));
-  //   },
-  // },
+  {
+    path: 'users/:userId/tasks',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const ids = ['u1', 'u2', 'u3'];
+      return ids.map((id) => ({ userId: id }));
+    },
+  },
   {
     path: '**',
     renderMode: RenderMode.Server,
